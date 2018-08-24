0f3faf2c39fbc480d3e17cc589d4870d09eac454
ooooottt <agoimukat@gmail.com>
2018-08-24 09:41:01 +0900

    KakeiboDaoに一覧表示の文を追加

--- Kakeibo/src/dao/KakeiboDao.java
+++ Kakeibo/src/dao/KakeiboDao.java
@@ -5,10 +5,83 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import dto.Kakeibo;
 
-public class KakeiboDao{
+	public class KakeiboDao {
+
+		public static ArrayList<Kakeibo> searchTable() {
+			ArrayList<Kakeibo> list = new ArrayList<>();
+
+			Connection con = null;
+			PreparedStatement ps = null;
+			ResultSet rs = null;
+
+			try {
+				//①JDBCドライバをロードする
+				Class.forName("com.mysql.jdbc.Driver");
+
+				//②DBMSとの接続を確立する
+				con = DriverManager.getConnection(
+						"jdbc:mysql://localhost:3306/kakeibo?useSSL=false",
+						"root",
+						"weeknd0216");
+
+				//③SQL文を作成する
+				String sql = "SELECT * FROM KAKEIBO;";
+
+				//④SQL文を実行するための準備をする
+				ps = con.prepareStatement(sql);
+
+				//⑤SQL文を実行してDBMSから結果を受信する
+				rs = ps.executeQuery();
+
+				//⑥実行結果を含んだインスタンスからデータを取り出す
+				while(rs.next() == true) {
+					int day = rs.getInt("day");
+					String name = rs.getString("name");
+					int price = rs.getInt("price");
+					list.add(new Kakeibo(day,name,price));
+				}
+
+			}catch(ClassNotFoundException e) {
+				System.out.println("JDBCドライバが見つかりません。");
+			}catch(SQLException e) {
+				System.out.println("DB接続時にエラーが発生しました。");
+				e.printStackTrace();
+			} finally {
+				// ⑦DBMSから切断する
+				try {
+					if(rs != null) {
+						rs.close();
+					}
+				}catch(SQLException e) {
+					System.out.println("DB切断時にエラーが発生しました。");
+					e.printStackTrace();
+				}
+
+				try {
+					if(ps != null) {
+						ps.close();
+					}
+				}catch(SQLException e) {
+					System.out.println("DB切断時にエラーが発生しました。");
+					e.printStackTrace();
+				}
+
+				try {
+					if(con != null) {
+						con.close();
+					}
+				}catch(SQLException e) {
+					System.out.println("DB切断時にエラーが発生しました。");
+					e.printStackTrace();
+				}
+			}
+
+			return list;
+		}
 
 	public static Kakeibo DetailView(int key){
 
